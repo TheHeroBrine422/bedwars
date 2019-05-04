fcb54f81cb3ad425474a8b750e2135b98c9d5364
Hero <caleb@calebgj.io>
2019-05-04 00:24:47 -0500

    add /helloworld

--- src/main/java/red/nulled/bedwars/BedWars.java
+++ src/main/java/red/nulled/bedwars/BedWars.java
@@ -1,6 +1,12 @@
 package red.nulled.bedwars;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.UUID;
 
 public final class BedWars extends JavaPlugin {
 
@@ -15,5 +21,15 @@ public final class BedWars extends JavaPlugin {
         // Plugin shutdown logic
         // test comment
         System.out.println("The server is stopping and the plugin is disabled!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("helloworld")) { // If the player typed /basic then do the following, note: If you only registered this executor for one command, you don't need this
+            //Player target = Bukkit.getServer().getPlayer(UUID.fromString("d03ea078-de10-4538-b696-b7fbbd2498e7"));
+            sender.sendMessage("Hello world");
+            return true;
+        }
+        return false;
     }
 }
