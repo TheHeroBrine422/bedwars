894e23b8c89d856617f352b2ab638f37da67fe73
ami <ami@amicole.co>
2019-05-04 00:05:29 -0500

    Update .gitignore and add test logging commands.

--- src/main/java/red/nulled/bedwars/BedWars.java
+++ src/main/java/red/nulled/bedwars/BedWars.java
@@ -7,12 +7,13 @@ public final class BedWars extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("The server has started and the plugin is enabled!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         // test comment
+        System.out.println("The server is stopping and the plugin is disabled!");
     }
 }
