e227fbd8cea64bdc53908a591e07aeebdec1c7a7
Hero <caleb@calebgj.io>
2019-05-04 00:46:05 -0500

    remove useless imports

--- src/main/java/red/nulled/bedwars/BedWars.java
+++ src/main/java/red/nulled/bedwars/BedWars.java
@@ -1,12 +1,8 @@
 package red.nulled.bedwars;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.UUID;
 
 public final class BedWars extends JavaPlugin {
 
